28cc14f6d4d4f821bf78a555c9d30c58abdbb326
nasghoi <nasrullahjamsarim@gmail.com>
2025-11-17 00:46:46 +0800

    add:: layout and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-// import { useState } from 'react'
+import Layout from "./components/Layout";
+import Grid from "./components/Grid";
+import Hero from "./components/Hero";
 
 function App() {
 
   return (
-    <>
-      <main class="flex m-4 gap-4">
-        <section>
-          <div class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
-            <div>
-              <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg"></span>
-            </div>
-            <h3 class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
-            <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-              The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-            </p>
-          </div>
-        </section>
-        <section>
-          <div class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
-            <div>
-              <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg"></span>
-            </div>
-            <h3 class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
-            <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-              The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-            </p>
-          </div>
-        </section>
+    <Layout>
+      <main>
+        <Hero />
+        { /* we make self closing tag since Grid doesnt have any CHILDREN */}
+        <Grid />
       </main>
-    </>
+    </Layout>
   )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,23 @@
-export default function Layout() {
+export default function Layout(props) {
+
+    const { children } = props
+    const header = (
+        <header>
+            <h1>GymMetry</h1>
+            <p><strong>The 30 Simple Workouts Program</strong></p>
+        </header>
+    )
+    const footer = (
+        <footer>
+            <p>Built by <a href="https://nasghoi.github.io/web-portfolio/" target="_blank">Nasghoi</a><br />Styled with <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a></p>
+        </footer>
+    )
+
     return (
-        <></>
+        <>
+            {header}
+            {children}
+            {footer}
+        </>
     )
 }
