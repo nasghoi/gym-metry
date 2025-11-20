749e953848eb140a8c4b3754457d080732d51421
nasghoi <nasrullahjamsarim@gmail.com>
2025-11-21 00:19:12 +0800

    feat: add workout card

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,39 +2,39 @@ import { workoutProgram as training_plan } from '../utils/index.js'
 import WorkoutCard from "./WorkoutCard.jsx";
 
 export default function Grid() {
-    const isLocked = true
-    const selectedWorkout = 0
+    const isLocked = false
+    const selectedWorkout = 2
 
     return (
         <div className="training-plan-grid">
             {Object.keys(training_plan).map((workout, workoutIndex) => {
                 const type = workoutIndex % 3 === 0 ? 'Push' : workoutIndex % 3 === 1 ? 'Pull' : 'Legs';
-                // const trainingPlan = training_plan[workoutIndex];
+                const trainingPlan = training_plan[workoutIndex];
+                const dayNum = ((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1
+                const icon = workoutIndex % 3 === 0 ? (
+                    <i className="fa-solid fa-dumbbell"></i>
+                ) : (
+                    workoutIndex % 3 === 1 ? (
+                        <i className="fa-solid fa-weight-hanging"></i>
+                    ) : (
+                        <i className="fa-solid fa-bolt"></i>
+                    )
+                )
 
                 if (workoutIndex === selectedWorkout) {
                     return (
-                        <WorkoutCard key={workoutIndex} />
+                        <WorkoutCard key={workoutIndex} trainingPlan={trainingPlan} type={type} workoutIndex={workoutIndex} dayNum={dayNum} icon={icon} />
                     )
                 }
 
                 return (
                     <button className={'btn-primary' + (isLocked ? ' inactive' : '')} key={workoutIndex}>
                         <div className="plan-card-header">
-                            <p>Day {((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1}</p>
+                            <p>Day {dayNum}</p>
                         </div>
                         {isLocked ? (
                             <i className="fa-solid fa-lock"></i>
-                        ) : (
-                            workoutIndex % 3 === 0 ? (
-                                <i className="fa-solid fa-dumbbell"></i>
-                            ) : (
-                                workoutIndex % 3 === 1 ? (
-                                    <i className="fa-solid fa-weight-hanging"></i>
-                                ) : (
-                                    <i className="fa-solid fa-bolt"></i>
-                                )
-                            )
-                        )}
+                        ) : icon}
                         <div className="plan-card-header">
                             <h5>{type}</h5>
                         </div>
--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -1,5 +1,32 @@
-export default function WorkoutCard() {
+export default function WorkoutCard(props) {
+    const { trainingPlan, workoutIndex, type, dayNum, icon } = props
+
+    const { warmup, workout } = trainingPlan || {}
+
     return (
-        <></>
+        <div className="workout-container">
+            <div className="workout-card card">
+                <div className="plan-card-header">
+                    <p>Day {dayNum}</p>
+                    {icon}
+                </div>
+                <div className="plan-card-header">
+                    <h2><b>{type}</b></h2>
+                </div>
+            </div>
+
+            <div className="workout-grid">
+                <div className="exercise-name">
+                    <h4>Warmup</h4>
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 className="weight-input">Max Weight </h6>
+                {warmup.map((warmupExercise, warmupIndex) => {
+
+                })}
+            </div>
+
+        </div>
     )
 }
